70a5daa140698c53be39ad7154a9c7bb1f855a02
narinsss <maarib03@gmail.com>
2022-07-08 22:13:38 -0300

    2

--- Java - Eclipse - Generation/Atv-Lacos-Repeticao/src/exercicios/LacosRepeticaoQuestao2.java
+++ Java - Eclipse - Generation/Atv-Lacos-Repeticao/src/exercicios/LacosRepeticaoQuestao2.java
@@ -6,20 +6,19 @@ public class LacosRepeticaoQuestao2 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
 		int num, par = 0, impar = 0;
 		System.out.println("Nos dê 10 números inteiros: ");
 		for(int i = 1; i <= 10; i++) {
 			num = scan.nextInt();
-			//else{
-//				impar = num;
-//			}
 			if(num % 2 == 0) {
-				num++;
-				System.out.println(num);
+				par++;
 			}
+	        if(num % 2 == 1){
+	            impar++;
+	        }
 	    }
 
-		System.out.println(par);
+		System.out.println("Você inseriu " + par + " números pares.");
+		System.out.println("Você inseriu " + impar + " números ímpares.");
 	}
 }
--- Java - Eclipse - Generation/Atv-Lacos-Repeticao/src/exercicios/fff.java
+++ Java - Eclipse - Generation/Atv-Lacos-Repeticao/src/exercicios/fff.java
@@ -1,5 +1,28 @@
 package exercicios;
 
-public class fff {
+import java.util.Scanner;
 
+public class fff {
+	public static void main(String[] args) {
+		  Scanner ent = new Scanner(System.in);
+	      int num, contPar = 0, contImpar = 0;
+	      
+	      for(int i = 1; i <= 5; i++){
+	          System.out.println("Digite o " + i + "º número");
+	          num = ent.nextInt();
+	          
+	          if(num % 2 == 0){
+	              contPar++;
+	          }
+
+	          // para informar números impares
+	          /*
+	          if(num % 2 == 1){
+	              contImpar++;
+	          }
+	          */
+	      }
+	      
+	      System.out.println("Foram digitados " + contPar + " números pares \n");
+	}
 }
