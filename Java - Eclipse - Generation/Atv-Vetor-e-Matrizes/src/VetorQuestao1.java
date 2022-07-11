f691ee6f5fd32468a7272a1d4b684b71ea2e0552
narinsss <maarib03@gmail.com>
2022-07-11 18:49:30 -0300

    duas questões em java sobre array unidimensional

---
+++ Java - Eclipse - Generation/Atv-Vetor-e-Matrizes/src/VetorQuestao1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VetorQuestao1 {
+
+	public static void main(String[] args) {
+		int[] notas = new int[5];
+		int nota = 0, maiorNota = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		for(int i = 0; i < 5; i++) {
+			int y = i + 1;
+			System.out.println("Digite o sua " + y +"° nota: ");
+			nota = scan.nextInt();
+			notas[i] = nota;
+			
+			if(maiorNota < nota) {
+				maiorNota = nota;
+			}
+		}
+		System.out.println("Notas: ");
+		for(int i = 0; i < 5; i++) {
+			System.out.print("[" + notas[i] + "]");
+		}
+		
+		System.out.println("\nMaior nota da turma: " + maiorNota);
+	}
+
+}
---
+++ Java - Eclipse - Generation/Projeto Generation 7-7/src/generation/VetorMatrizesQuartaAula.java
@@ -0,0 +1,10 @@
+package generation;
+
+public class VetorMatrizesQuartaAula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
